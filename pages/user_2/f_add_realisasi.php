54b9f8627fad03f537462b74b5886b450202ff86
Arka Arifiandi Leonanta <arkaarif69@gmail.com>
2021-02-04 06:30:29 +0700

    Update f_add_realisasi.php

--- pages/user_2/f_add_realisasi.php
+++ pages/user_2/f_add_realisasi.php
@@ -1,29 +1,33 @@
 <?php
 
-session_start();
-error_reporting(0);
+    include('../../function/koneksi.php');
 
-$connect = mysqli_connect("localhost", "root", "", "magang_pal");
+    function resizeImage($resourceType,$image_width,$image_height,$resizeWidth,$resizeHeight) {
+        $imageLayer = imagecreatetruecolor($resizeWidth,$resizeHeight);
+        imagecopyresampled($imageLayer,$resourceType,0,0,0,0,$resizeWidth,$resizeHeight, $image_width,$image_height);
+        return $imageLayer;
+    }
 
     if(!empty($_POST)){
         $output = '';
-        $id_plan = mysqli_real_escape_string($_POST["id_plan"]);
-        $id_task = mysqli_real_escape_string($_POST["id_task"]);  
-        $task = mysqli_real_escape_string($_POST["task"]);  
-        $date = mysqli_real_escape_string($_POST["date"]);  
-        $plan = mysqli_real_escape_string($_POST["plan"]);
-        $status = mysqli_real_escape_string($_POST["status"]);
-        $kendala = mysqli_real_escape_string($_POST["kendala"]);
+        $id_plan = $_POST["id_plan"];
+        $id_task = $_POST["id_task"];  
+        $task = $_POST["task"];  
+        $date = $_POST["date"];  
+        $plan = $_POST["plan"];
+        $status = $_POST["status"];
+        $kendala = $_POST["kendala"];
         
-		$query = "INSERT INTO tabel_realisasi SET id_plan= '$id_plan', id_task = '$id_task', date ='$date', 
-													plan='$plan', status='1', bukti='', kendala='$kendala'";
-        $query2="UPDATE tabel_plan SET status = '1' WHERE id_plan = '$id_plan'";
+		$query = ("INSERT INTO tabel_realisasi(id_plan,id_task,date,plan,status,bukti,kendala)
+		VALUES ('".$id_plan."', '".$id_task."','".$date."','".$plan."',
+				'1','','$kendala')");
 
-        if(mysqli_query($connect, $query)){
-			mysqli_query($connect, $query2) ?>; 
-			<script type="text/javascript"> alert("Realisasi berhasil !"); </script>
+        $query2="UPDATE tabel_plan SET status = '1' WHERE id_plan = '".$id_plan."'";
+
+        if(mysqli_query($conn, $query)){  
+			mysqli_query($conn, $query2)?>
+			<script type="text/javascript"> alert("Task berhasil ditambahkan !"); </script>
         	<?php echo"<script>document.location='index.php?plan'</script>"; ?>
 		<?php }
-		echo $output;
     }
 ?>
